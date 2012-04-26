f26257e5c9e884049893aef03afc7fe94b2667a8
John Floren <john@jfloren.net>
2012-04-26 14:57:17 -0700

    Linux now seems to be working; I ran it with 100 clients doing gets and sets, didn't experience any crashes. Gets are still oddly slow (on the order of 20 per second), may be the implementation of rwlocks in pthreads?

--- linux.c
+++ linux.c
@@ -4,71 +4,98 @@
 
 #define MAXCLIENTS 1024
 
+// This is a silly thing pthreads does
+typedef struct pClient pClient;
+struct pClient {
+	int fd;
+};
+
+pthread_rwlock_t threadslock;
 pthread_t threads[MAXCLIENTS];
+pClient *clients[MAXCLIENTS];
 
 int sock;
 
 void*
 handler_wrapper(void *arg)
 {
-	int *fd;
+	pClient *c;
+	int fd;
 
-	fd = (int*)arg;
-	handler(*fd);
+	c = (pClient*)arg;
+	fd = c->fd;
+	handler(fd);
 }
 
+// This will clean up threads which have exited.
 void*
 reaper(void *arg)
 {
-	int i;
+	int i, joined;
 	void *ret;
 	for (;;) {
+		wlock(&threadslock);
 		for (i = 0; i < MAXCLIENTS; i++) {
 			if (threads[i] != NULL) {
-				pthread_tryjoin_np(threads[i], &ret);
+				joined = pthread_tryjoin_np(threads[i], &ret);
+				if (joined == 0) {
+					threads[i] = NULL;
+					free(clients[i]);
+				}
 			}
 		}
+		wunlock(&threadslock);
+		sleep(2);
 	}
 }
 
 void
 main()
 {
-        int fd, i;
-        int on = 1;
-        struct sockaddr_in servaddr;
+	int fd, i;
+	int on = 1;
+	struct sockaddr_in servaddr;
+	pthread_t reaperthread;
 
-	ht = inittable(1);
+	ht = inittable(1024);
 
 	pthread_rwlock_init(&ht->l, NULL);
+	pthread_rwlock_init(&threadslock, NULL);
 
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock < 0) {
-                printf("something wicked happened\n");
-                exit(-1);
-        }
-        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		printf("something wicked happened\n");
+		exit(-1);
+	}
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-        memset(&servaddr, 0, sizeof(servaddr));
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        servaddr.sin_port = htons(6379);
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(6379);
 
-        bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-        listen(sock, 100);
+	listen(sock, 100);
+
+	// Start the harvester of threads
+	pthread_create(&reaperthread, NULL, reaper, NULL);
 
 	for (;;) {
 		fd = accept(sock, NULL, NULL);
+
 		if (fd < 0)
 			exits("listen failed");
 
+		wlock(&threadslock);
 		for (i = 0; i < MAXCLIENTS; i++) {
 			if (threads[i] == NULL) { 
-				pthread_create(&threads[i], NULL, handler_wrapper, &fd);
+				clients[i] = (pClient*)malloc(sizeof(pClient));
+				clients[i]->fd = fd;
+				pthread_create(&threads[i], NULL, handler_wrapper, (void*)clients[i]);
 				break;
 			}
 		}
-	
+		wunlock(&threadslock);
 	}
 }
